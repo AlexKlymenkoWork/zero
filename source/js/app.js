194756e29b56c4a51fbd2a7ab847ab04e4674487
AlexKlm <oklymenko@edu.cdv.pl>
2018-07-30 22:14:56 +0200

    add animation

--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,63 @@
 
 // JavaScript
-window.sr = ScrollReveal(); 
+window.sr = ScrollReveal();
+sr.reveal('article', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#features h2', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px'
+});
+sr.reveal('#features div.second', {
+    delay: 300,
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#features div.third', {
+    delay: 600,
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#features div.fourth', {
+    delay: 900,
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#description img', {
+    delay: 300,
+    duration: 2000,
+    origin: 'left',
+    distance: '50px'
+});
+sr.reveal('#description div.info-text', {
+    delay: 600,
+    duration: 2000,
+    origin: 'rigth',
+    distance: '50px'
+});
+sr.reveal('#contact h2', {
+    delay: 300,
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px'
+});
+sr.reveal('#contact div.contact-form', {
+    delay: 600,
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px'
+});
 
-// sr.reveal('h1', {
-//     delay: 0,
-//     duration: 200,
-//     origin: 'bottom',
-//     distance: '100px' 
-// });
+
+
+
+
 
 var navButton = document.getElementById("nav-toggle-button");
 
